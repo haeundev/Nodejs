fbafe7782025025cc47533ea6298283d8fe9053d
haeundev <haeundev@gmail.com>
2024-11-19 00:57:34 +0900

    Enhance event logging by passing arguments to the listener

--- first-app/app.js
+++ first-app/app.js
@@ -4,9 +4,9 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 // Register a listener
-emitter.on('messageLogged', function() {
-    console.log('Listener called');
+emitter.on('messageLogged', function(arg) {
+    console.log('Listener called', arg);
   });
 
 // Raise an event
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', { id: 1, url: 'http://' });
